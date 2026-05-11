0ac1c1da4a3c1db77e722acf65ba1ad8bba9a50b
Zeeshan <sk.zeeshan1992@gmail.com>
2026-05-10 20:17:37 -0400

    add deploy config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
 import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
 import { errorHandler } from './middleware/errorHandler.js'
 import authRoutes from './routes/authRoute.js'
 import chatRoutes from './routes/chatRoute.js'
@@ -8,6 +10,9 @@ import messageRoutes from './routes/messageRoute.js'
 import userRoutes from './routes/userRoute.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const webDistPath = path.resolve(__dirname, '../../web/dist')
 
 app.use(cors({}))
 app.use(express.json())
@@ -20,11 +25,7 @@ app.use(clerkMiddleware())
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Server is healthy' })
-
-
 })
-
-
 
 app.use('/api/auth', authRoutes)
 app.use('/api/chats', chatRoutes)
@@ -33,6 +34,12 @@ app.use('/api/users', userRoutes)
 
 app.use(errorHandler)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(webDistPath))
+
+  app.get('/{*any}', (_, res) => {
+    res.sendFile(path.join(webDistPath, 'index.html'))
+  })
+}
+
 export default app
-
-
